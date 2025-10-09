2a5fc3d55ca66ec6e4ee256772022fb41b9b0581
Zzebb <blackmorea2@gmail.com>
2025-10-09 10:02:01 +0100

    idk what im pushing

--- queries/chart1.sql
+++ queries/chart1.sql
@@ -1,3 +1,3 @@
 .mode json
 
-SELECT COUNT(tracks.explicit) AS explicit_track_count, artists.name AS artist_name FROM albums INNER JOIN artists ON artists.id = albums.artist_id INNER JOIN tracks on tracks.album_id = albums.id WHERE tracks.explicit GROUP BY artists.id ORDER BY COUNT(tracks.explicit) DESC;
+SELECT COUNT(tracks.explicit) AS explicit_tra   ck_count, artists.name AS artist_name FROM albums INNER JOIN artists ON artists.id = albums.artist_id INNER JOIN tracks on tracks.album_id = albums.id WHERE tracks.explicit GROUP BY artists.id ORDER BY COUNT(tracks.explicit) DESC;
